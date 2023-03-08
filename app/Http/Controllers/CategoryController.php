f6b5b4816ff62b2d6aacaaf033f9081383343d36
NguyenThu6123 <nguyenthu612300@gmail.com>
2023-03-08 16:42:21 +0700

    update8-3

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,67 +2,111 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         //
         $categoryList = Category::all();
-        return view('categories.index',['categoryList' =>$categoryList]);
+        return view('categories.index', ['categoryList' => $categoryList]);
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         //
+        return view('categories.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $category = Category::create($request->only(['name','desc']));
+        $message = "Thêm mới thành công";
+        if($category == null){
+            $message = "thêm mới thất bại";
+        }
+        return redirect()->route('categories.index')->with('message', $message);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(string $id)
+    public function show($id)
     {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(string $id)
+    public function edit($id)
     {
         //
+        $category = Category::findOrFail($id);
+        return view('categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         //
+        $category = Category::findOrFail($id);
+        $bool = $category->update($request->only(['name','desc']));
+        $message = "Cập nhật thành công";
+        if(!$bool){
+            $message = "cập nhật thất bại";
+
+        }
+        return redirect()->route('categories.index')->with('message', $message);
+       
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
         //
+        $message = "Xóa thành công";
+        if (!Category::destroy($id)) {
+            $message = "xóa thất bại";
+        }
+
+        return redirect()->route('categories.index')->with('message', $message);
     }
 }
